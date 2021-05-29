8457e7f98913b44a36be148c93b56c0e807cf7e4
pankajmature <pankaj.mature@outlook.com>
2021-05-29 06:21:13 +0530

    New changes 432trsfgxyf

--- Program.cs
+++ Program.cs
@@ -13,9 +13,9 @@ namespace GrpcGreeterClient
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client =  new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
-                              new HelloRequest { Name = "GreeterClient" });
+                              new HelloRequest { Name = "Pankaj" });
             Console.WriteLine("Greeting: " + reply.Message);
-            Console.WriteLine("Press any key to exit... Hiii");
+            Console.WriteLine("Press any key to exit...");
             Console.Read();
         }
     }
